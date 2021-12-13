64bae26fd31b1cd6894a39e01ea854ca08fface3
fuliang <632249982@qq.com>
2021-12-13 13:41:15 +0800

    提交

--- php8/zhujie.php
+++ php8/zhujie.php
@@ -10,28 +10,3 @@ function show($name)
 {
 }
 
-//$ref = new \ReflectionFunction("show");
-//$doc = $ref->getDocComment();
-//$api = substr($doc, strpos($doc, "@api") + strlen("@api "), -2);
-//print_r($api);
-//$session = 11;
-//echo $session?->user?->getAddress()?->country;
-//$name = match ($request_method) {
-//    'post' => $this->handlePost(),
-//    'get', 'head' => $this->handleGet(),
-//};
-//echo $name;
-/*$code = '<?php echo 123 ?>';*/
-//$arr = token_get_all($code);
-//$arr=\PhpToken::tokenize($code);
-//var_dump($arr);
-//try {
-//    substr('linux', []);
-//} catch (\TypeError $e) {
-//    echo $e->getMessage();
-//}
-//try {
-//    json_decode('"foo"', true, -1);
-//} catch (\ValueError $e) {
-//    echo $e->getMessage();
-//}
