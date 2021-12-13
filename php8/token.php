3fbcd1c2b2a063f0554c08d29358f5f6e61a8948
fuliang <632249982@qq.com>
2021-12-13 11:30:22 +0800

    提交

--- php8/token.php
+++ php8/token.php
@@ -5,4 +5,10 @@ namespace php8;
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 //$str = 1/0;
-var_dump(fdiv(1, 0));
+//var_dump(fdiv(6, 2));
+$file = 'img/WechatIMG29.png';
+$f = fopen($file, 'rw');
+var_dump($f);
+$fi = (int)$f;
+var_dump($fi);
+var_dump(get_resource_id($f));
