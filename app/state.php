c047d7d05de8bdc9a91fd7308375b3b8e7eb7863
fuliang <fuliang19890809@163.com>
2022-06-29 16:26:17 +0800

    提交

--- app/state.php
+++ app/state.php
@@ -1,10 +1,15 @@
 <?php
 /**
  * 状态机模式
+ * @description 可以根据不同状态或者消息类型进行相应的处理逻辑，使得程序逻辑清晰易懂
  */
+namespace app;
+
 use Runner\Heshen\Machine;
 use common\state\Document;
 use common\state\Graph;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $machine = new Machine(new Document(), new Graph());
 
@@ -24,14 +29,14 @@ var_dump($machine->can('two', ['number' => 6])); // output: bool(true)
 
 //通过 Factory 获取 Machine
 
-use Runner\Heshen\Factory;
-
-$factory = new Factory([
-    Document::class => Graph::class,
-]);
-
-$document = new Document;
-
-$machine = $factory->make($document);
-
-var_dump($machine->can('one')); // output: bool(true)
+//use Runner\Heshen\Factory;
+//
+//$factory = new Factory([
+//    Document::class => Graph::class,
+//]);
+//
+//$document = new Document;
+//
+//$machine = $factory->make($document);
+//
+//var_dump($machine->can('one')); // output: bool(true)
